7b3f45ca2e255cd91e237d4f1a3257cc96f0f447
James Montalvo <edwin.j.montalvo@nasa.gov>
2015-09-23 23:01:31 -0500

    add comments

--- benchmark.php
+++ benchmark.php
@@ -1,31 +1,29 @@
 <?php
 
+// get time in milliseconds of script start
 $scriptStart = microtime( true );
 
-
+// benchmark configuration
 $numQueries = $_GET['num_queries'];
 $numIds = $_GET['num_ids'];
 $queryTable = "text";
 $idColumn = "old_id";
-
 $numPhpLoops = $_GET['php_loops'];
 
 // setup mysql connection
 require_once __DIR__ . '/conf.php';
-
 $db = mysql_connect(
 	$conf['host'],
 	$conf['user'],
 	$conf['pass']
 ) or die ('Unable to connect. Check you connection parameters.');
-
 mysql_select_db( $conf['db_name'], $db ) or die( mysql_error( $link ) );
 
-
-
-
+// get time of query start
 $queryStart = microtime( true );
 
+// do a bunch of loops, repeating similar queries (but randomly different
+// so caching shouldn't take effect)
 for( $i = 0; $i < $numQueries; $i++ ) {
 
 	$ids = array();
@@ -39,20 +37,22 @@ for( $i = 0; $i < $numQueries; $i++ ) {
 
 }
 
+// record length of database test
 $queryLength = microtime( true ) - $queryStart;
 
-
-
+// record start of php test
 $phpStart = microtime( true );
 
+// do a bunch of loops (thousands or millions) doing a fairly complex task
+// of generating a cryptographic hash.
 for( $k = 0; $k < $numPhpLoops; $k++ ) {
-
 	$list = sha1( $list );
-
 }
 
+// record php test length
 $phpLength = microtime( true ) - $phpStart;
 
+// respond with data
 header('Content-Type: application/json');
 $data = array(
 	"script" => round( microtime( true ) - $scriptStart, 3),
